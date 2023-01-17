b2e3da3b9639484d771ddbec3656d7b0c7345141
cordu69420 <rolandvtuber@gmail.com>
2023-01-17 20:46:01 +0200

    rescalling project added

---
+++ Image rescalling/build/CMakeFiles/ImageRescalling.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImageRescalling.dir/main.cpp.o"
+  "ImageRescalling"
+  "ImageRescalling.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImageRescalling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Image rescalling/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cordu/Image Processing/Exercises/Image rescalling
+# Build directory: /home/cordu/Image Processing/Exercises/Image rescalling/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
