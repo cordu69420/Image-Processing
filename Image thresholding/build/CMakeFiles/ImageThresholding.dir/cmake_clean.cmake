b56a6fc315ba6109ed0e08d9ce9ac94fcb0b1202
codrut69420 <rolandvtuber@gmail.com>
2023-02-18 21:16:06 +0200

    Added morphological and color chapters

---
+++ Image thresholding/build/CMakeFiles/ImageThresholding.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImageThresholding.dir/main.cpp.o"
+  "ImageThresholding"
+  "ImageThresholding.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImageThresholding.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Image thresholding/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cordu/Image-Processing/Image thresholding
+# Build directory: /home/cordu/Image-Processing/Image thresholding/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
