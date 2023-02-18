e78f3f92fc50297f5c53d738972f22b578a0263c
codrut69420 <rolandvtuber@gmail.com>
2023-02-18 21:40:21 +0200

    Removed build and config directories

--- Color domain processing/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cordu/Image-Processing/Color domain processing
-# Build directory: /home/cordu/Image-Processing/Color domain processing/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
