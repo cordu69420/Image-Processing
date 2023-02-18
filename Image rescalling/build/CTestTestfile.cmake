eece7bb7f4d8dc1e2e21ed570de4b1937118b98b
codrut69420 <rolandvtuber@gmail.com>
2023-02-18 21:28:35 +0200

    Added gitignore

--- Image rescalling/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cordu/Image Processing/Exercises/Image rescalling
-# Build directory: /home/cordu/Image Processing/Exercises/Image rescalling/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
